2e28d01a05adf6c5276778727cbb41d1469081ca
aFoxNamedMorris <afoxnamedmorris@gmail.com>
2016-09-27 02:17:00 -0400

    Bring codebase up to date.

--- GrabSong.py
+++ GrabSong.py
@@ -1,28 +1,88 @@
 #!/usr/bin/env python
 from subprocess import Popen, PIPE
-
+import os
 
 class GrabSong(object):
     def __init__(self, player):
+        self.start = True
         self.player = player
-        self.metadata = self.get_metadata(player)
+        self.metadata = self.get_metadata()
+        self.curdir = os.path.dirname(os.path.realpath(__file__))
+        self.outdir = "%s/Output" % self.curdir
+        self.outfiles = ["artist", "album", "title"]
+        self.song_art = None
 
-    def get_metadata(self, player):
-        data = Popen(["qdbus", "org.mpris.MediaPlayer2.%s" % player, "/org/mpris/MediaPlayer2", "org.mpris.MediaPlayer2.Player.Metadata"], stdout=PIPE)
+    def get_metadata(self):
+        data = Popen(
+            [
+                "qdbus",
+                "org.mpris.MediaPlayer2.%s" % self.player,
+                "/org/mpris/MediaPlayer2",
+                "org.mpris.MediaPlayer2.Player.Metadata"
+            ],
+            stdout=PIPE
+        )
         return self._parse_metadata(data)
+
+    def save(self):
+        try:
+            os.makedirs(self.outdir)
+        except OSError:
+            if os.path.isdir(self.outdir):
+                pass
+            else:
+                raise
+
+        for name in self.outfiles:
+            with open("%s/%s.txt" % (self.outdir, name), "w") as f:
+                f.write(self.metadata[name])
+        if not self.song_art:
+            self.song_art = "%s/Images/NoArt.jpg" % self.curdir
+
+        Popen(
+            [
+                "convert",
+                self.song_art, "-resize", "500x500!",
+                self.outdir + "/AlbumArt.jpg"
+            ]
+        )
+
+    def update(self):
+        try:
+            try:
+                last = self.metadata["title"]
+            except (TypeError, KeyError):
+                last = None
+            self.metadata = self.get_metadata()
+
+            for typ in self.outfiles:
+                try:
+                    if self.metadata[typ]:
+                        pass
+                except KeyError:
+                    self.metadata[typ] = "No information available."
+
+            if last != self.metadata["title"] or self.start is True:
+                self.start = False
+                self.save()
+        except KeyError:
+            self.metadata = "No valid metadata detected."
 
     def _parse_metadata(self, data):
         data = data.stdout.read().decode("UTF-8")
         data = data.split("\n")
         data_dict = {}
-        ignore = ["http", "url", "contentCreated"]
+        ignore = ["comment", "url", "contentCreated"]
 
         for datum in data:
+            if "mpris:artUrl:" in datum:
+                self.song_art = datum[14:]  # Removes "mpris:artUrl:"
+#            else:
+#                self.song_art = None
             if "xesam" in datum:
-                d = datum[6:]  # get rid of "xesam:"
-                d = d.split(":")
+                d = datum[6:].split(":")  # Removes "xesam:"
                 if d[0] not in ignore:
-                    data_dict[d[0]] = d[1]
+                    data_dict[d[0]] = d[1].strip().encode("utf-8")
                 
         return data_dict
 
--- run.py
+++ run.py
@@ -1,9 +1,55 @@
 #!/usr/bin/env python
 from GrabSong import GrabSong
+import curses
+import locale
+from time import time, sleep
 
 
-player = GrabSong("clementine")
+# Add these to a config file
+debug = False
+version = "0.0.1"
+interval = 1
+locale.setlocale(locale.LC_ALL, "")
+# End Add these to a config file
 
-print("Title: %s" % player.metadata["title"])
-print("Artist: %s" % player.metadata["artist"])
-print("Album: %s" % player.metadata["album"])
+player = GrabSong("pithos")
+
+def validate():
+    valid = ["title", "artist", "album"]
+    for v in valid:
+        if type(v) is str:
+            player.metadata[valid] = "Invalid metadata."
+
+def set_strings(screen):
+    status = player.update()
+    screen.erase()
+    screen.border(1)    
+    screen.addstr(1, 2, "pygrab-song %s" % version, curses.color_pair(1))
+    if type(status) is not str:
+#        validate()
+        if type(player.metadata) is not str:
+            screen.addstr(3, 2, "Title: %s" % player.metadata["title"])
+            screen.addstr(5, 2, "Artist: %s" % player.metadata["artist"])
+            screen.addstr(7, 2, "Album: %s" % player.metadata["album"])
+        else:
+            screen.addstr(3, 2, player.metadata)
+        if debug:
+            screen.addstr(9, 2, "[DEBUG] Time: %s" % time())
+            screen.addstr(10, 2, "[DEBUG] %s" % player.song_art)
+    else:
+        screen.addstr(3, 2, "This track has invalid metadata.")
+
+    screen.refresh()
+
+def main(screen):
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    while True:
+        sleep(interval)
+        set_strings(screen)
+    curses.endwin()
+
+if __name__ == "__main__":
+    try:
+        curses.wrapper(main)
+    except (KeyboardInterrupt, SystemExit):
+        curses.endwin()
